e49e983bce2e8797bd501223e2dd0940fcd4e265
yourusernameCave <dracothedragon9292@gmail.com>
2025-07-05 15:08:53 -0600

    comit

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,27 +5,31 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Enter a grade (0-100):");
-        int Grade = int.Parse(Console.ReadLine());
+        int Letter = int.Parse(Console.ReadLine());
 
-        if (Grade >= 90)
+        if (Letter >= 90)
         {
             Console.WriteLine("A");
         }
-        else if (Grade >= 80)
+        else if (Letter >= 80)
         {
             Console.WriteLine("B");
         }
-        else if (Grade >= 70)
+        else if (Letter >= 70)
         {
             Console.WriteLine("C");
         }
-        else if (Grade >= 60)
+        else if (Letter >= 60)
         {
             Console.WriteLine("D");
         }
         else
         {
             Console.WriteLine("F");
+        }
+        if (Letter >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed!");
         }
     }
 }
--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,30 @@
 using System;
-
+//Thomas Barney
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random rnd = new Random();
+        int randomNumber = rnd.Next(1, 101);
+        int guess = 0;
+
+        while (guess != randomNumber)
+        {
+            Console.Write("Guess the number (1-100): ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess == randomNumber)
+            {
+                Console.WriteLine("Congratulations! You guessed the right number.");
+            }
+            else if (guess < randomNumber)
+            {
+                Console.WriteLine("Your guess is too low.");
+            }
+            else if (guess > randomNumber)
+            {
+                Console.WriteLine("Your guess is too high.");
+            }
+        }
     }
 }
