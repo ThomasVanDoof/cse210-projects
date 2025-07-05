31399834b036b044e29aa3458e7e52bfbe936b62
yourusernameCave <dracothedragon9292@gmail.com>
2025-07-05 16:08:24 -0600

    commit

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,34 @@
 using System;
-
+using System.Collections.Generic;
+using System.Linq;
+// Thomas Barney
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        int number = -1;
+
+        
+        while (number != 0)
+        {
+            Console.Write("Enter a number (0 to quit): ");
+            number = int.Parse(Console.ReadLine());
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        }
+
+        int sum = numbers.Sum();
+        Console.WriteLine($"The sum is: {sum}");
+
+        
+        double average = numbers.Count > 0 ? numbers.Average() : 0;
+        Console.WriteLine($"The average is: {average}");
+
+        
+        int max = numbers.Count > 0 ? numbers.Max() : 0;
+        Console.WriteLine($"The largest number is: {max}");
     }
 }
--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        Console.WriteLine("Welcome to the program!");
+        Console.WriteLine("Enter your name:");
+        string name = Console.ReadLine();
+        Console.WriteLine("Enter your favorite number:");
+        int favoriteNumber = int.Parse(Console.ReadLine());
+
+        int square = favoriteNumber * favoriteNumber;
+        Console.WriteLine($"Hello {name}, your favorite number is {favoriteNumber}.");
+        Console.WriteLine($"The square of your favorite number is {square}.");
     }
 }
