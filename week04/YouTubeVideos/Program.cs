233ecc52033233836e616b2bc59a02b39e5ea8c0
yourusernameCave <dracothedragon9292@gmail.com>
2025-07-22 17:17:51 -0600

    o

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,9 +1,94 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+namespace YouTubeVideoTracker
 {
-    static void Main(string[] args)
+    public class Comment
     {
-        Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+        public string CommenterName { get; set; }
+        public string Text { get; set; }
+
+        public Comment(string commenterName, string text)
+        {
+            CommenterName = commenterName;
+            Text = text;
+        }
+    }
+
+    public class Video
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int LengthInSeconds { get; set; }
+        private List<Comment> comments;
+
+        public Video(string title, string author, int lengthInSeconds)
+        {
+            Title = title;
+            Author = author;
+            LengthInSeconds = lengthInSeconds;
+            comments = new List<Comment>();
+        }
+
+        public void AddComment(Comment comment)
+        {
+            comments.Add(comment);
+        }
+
+        public int GetCommentCount()
+        {
+            return comments.Count;
+        }
+
+        public List<Comment> GetComments()
+        {
+            return comments;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Video> videos = new List<Video>();
+
+            Video video1 = new Video("Protogen Chronicals Trailer", "Dr. Hare Studios", 75);
+            video1.AddComment(new Comment("Alice", "*sniff* ... *cough* ... *sniff*"));
+            video1.AddComment(new Comment("Bob_TheUnmoving", "The graphics look way better than I thought they were going to be."));
+            video1.AddComment(new Comment("CharlieBarly", "I can't wait to play this game!"));
+            videos.Add(video1);
+
+            Video video2 = new Video("DevLog 0", "Dr. Hare Studios", 900);
+            video2.AddComment(new Comment("DanaTheExplorer", "Def gunna pre-order fr fr!"));
+            video2.AddComment(new Comment("Eccles_justin_hole_digger", "Can you add a hole digging mechanic?"));
+            video2.AddComment(new Comment("Lank", "Exciting."));
+            videos.Add(video2);
+
+            Video video3 = new Video("DevLog 01", "Dr. Hare Studios", 1200);
+            video3.AddComment(new Comment("Gracius_Grace", "Your characters are so beautiful!"));
+            video3.AddComment(new Comment("HankLeTank093", "Having renditions really helped me picture the idea in my mind."));
+            video3.AddComment(new Comment("09PoisonIvy90", "What game engine are you going to use?"));
+            videos.Add(video3);
+
+            Video video4 = new Video("DevLog 02", "Dr. Hare Studios", 1600);
+            video4.AddComment(new Comment("Chathanyell0984", "bro u suck."));
+            video4.AddComment(new Comment("KarKaroneeightyfour", "Some advice might be to seperate the difficulty class from the charicter creation."));
+            video4.AddComment(new Comment("LeonTheStarry", "Impressive, I might do this for my own game."));
+            videos.Add(video4);
+
+            foreach (var video in videos)
+            {
+                Console.WriteLine($"Title: {video.Title}");
+                Console.WriteLine($"Author: {video.Author}");
+                Console.WriteLine($"Length: {video.LengthInSeconds} seconds");
+                Console.WriteLine($"Number of Comments: {video.GetCommentCount()}");
+                Console.WriteLine("Comments:");
+                foreach (var comment in video.GetComments())
+                {
+                    Console.WriteLine($"  - {comment.CommenterName}: {comment.Text}");
+                }
+                Console.WriteLine(new string('-', 50));
+            }
+        }
     }
 }
